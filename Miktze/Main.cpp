e72110e35ad2e03766f154be284ef20403912868
uri <13288uri@gmail.com>
2025-06-23 12:59:50 +0300

    Fix tabbing?

--- Miktze/Main.cpp
+++ Miktze/Main.cpp
@@ -5,17 +5,17 @@ using std::cout;
 using std::endl;
 
 int main() {
-	int* x = new int;
-	double* y = new double;
-	void* z = operator new (1337);
-	printAttachments();
-	delete(x);
-	cout << "Deleted first allocation" << endl;
-	printAttachments();
-	delete(y);
-	delete(z);
-	cout << "Deleted all allocations" << endl;
-	printAttachments();
+    int* x = new int;
+    double* y = new double;
+    void* z = operator new (1337);
+    printAttachments();
+    delete(x);
+    cout << "Deleted first allocation" << endl;
+    printAttachments();
+    delete(y);
+    delete(z);
+    cout << "Deleted all allocations" << endl;
+    printAttachments();
 
-	return 0;
+    return 0;
 }
--- Miktze/Miktze.cpp
+++ Miktze/Miktze.cpp
@@ -12,77 +12,77 @@ using std::string;
 Attachment* attachments = nullptr;
 
 Attachment::Attachment(size_t AllocationSize) : m_allocationSize(AllocationSize), m_next(nullptr) {
-	// Left blank intentionally
+    // Left blank intentionally
 }
 
 size_t Attachment::getAllocationSize() const {
-	return m_allocationSize;
+    return m_allocationSize;
 }
 
 Attachment* Attachment::getNext() const {
-	return m_next;
+    return m_next;
 }
 
 void Attachment::setAllocationSize(size_t allocationSize) {
-	m_allocationSize = allocationSize;
+    m_allocationSize = allocationSize;
 }
 
 void Attachment::setNext(Attachment* next) {
-	m_next = next;
+    m_next = next;
 }
 
 void* operator new(size_t size) {
-	Attachment* newAttachment = (Attachment*)(malloc(sizeof(*newAttachment) + size));
-	if (!newAttachment) {
-		throw bad_alloc();
-	}
+    Attachment* newAttachment = (Attachment*)(malloc(sizeof(*newAttachment) + size));
+    if (!newAttachment) {
+        throw bad_alloc();
+    }
 
-	newAttachment->setAllocationSize(size);
-	newAttachment->setNext(nullptr);
+    newAttachment->setAllocationSize(size);
+    newAttachment->setNext(nullptr);
 
-	if (!attachments) {
-		attachments = newAttachment;
-	} else {
-		Attachment* node = attachments;
-		while (node->getNext()) {
-			node = node->getNext();
-		}
-		node->setNext(newAttachment);
-	}
-	return (void*)(newAttachment + 1);
+    if (!attachments) {
+        attachments = newAttachment;
+    } else {
+        Attachment* node = attachments;
+        while (node->getNext()) {
+            node = node->getNext();
+        }
+        node->setNext(newAttachment);
+    }
+    return (void*)(newAttachment + 1);
 }
 
 void operator delete(void* pointer) {
-	if (!attachments || !pointer) {
-		throw BadFree();
-	}
-	Attachment* attachment = (Attachment*)pointer - 1;
-	Attachment* node = attachments;
+    if (!attachments || !pointer) {
+        throw BadFree();
+    }
+    Attachment* attachment = (Attachment*)pointer - 1;
+    Attachment* node = attachments;
 
-	if (node == attachment) {
-		attachments = attachments->getNext();
-		free(attachment);
-		return;
-	}
-	while (node) {
-		if (node->getNext() == attachment) {
-			node->setNext(attachment->getNext());
-			free(attachment);
-				return;
-		}
-		node = node->getNext();
-	}
-	throw BadFree();
+    if (node == attachment) {
+        attachments = attachments->getNext();
+        free(attachment);
+        return;
+    }
+    while (node) {
+        if (node->getNext() == attachment) {
+            node->setNext(attachment->getNext());
+            free(attachment);
+            return;
+        }
+        node = node->getNext();
+    }
+    throw BadFree();
 }
 
 void printAttachments() {
-	Attachment* node = attachments;
-	while (node) {
-		cout << "Address: " << node + 1 << " Size: " << node->getAllocationSize() << endl;
-		node = node->getNext();
-	}
+    Attachment* node = attachments;
+    while (node) {
+        cout << "Address: " << node + 1 << " Size: " << node->getAllocationSize() << endl;
+        node = node->getNext();
+    }
 }
 
 BadFree::BadFree() {
-	cout << "Cannot free memory that was not allocated using malloc" << endl;
+    cout << "Cannot free memory that was not allocated using malloc" << endl;
 }
